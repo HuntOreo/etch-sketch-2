0c8710244205bfdc8dd1ec1af03851cf26a1cdbc
HuntOreo <hloreair@outlook.com>
2025-08-02 01:21:59 -0400

    Fix syntax

--- script.js
+++ script.js
@@ -3,26 +3,25 @@ const board = document.querySelector('.board');
 function buildBoard() {
   const boardSize = 50;
 
-  // Build board, one row at a time.
+  // Build board, one column at a time.
   for (let i = 0; i < boardSize; i++) {
-    const row = document.createElement('div');
-    row.classList.toggle('row');
+    const column = document.createElement('div');
+    column.classList.toggle('column');
     for (let j = 0; j < boardSize; j++) {
       const pixel = document.createElement('div');
 
       // Listen for when a pixel is painted.
-      pixel.addEventListener('click', event => fillPixel(event.target))
-      pixel.addEventListener('mouseover', (event) => {
-        if (event.buttons == 1) {
-          fillPixel(event.target);
-        }
+      pixel.addEventListener('click', event => fillPixel(event.target));
+      pixel.addEventListener('mouseover', event => {
+        const isMouseClicked = event.buttons === 1 ? true : false;
+        if (isMouseClicked) { fillPixel(event.target); };
       });
 
-      row.appendChild(pixel);
+      column.appendChild(pixel);
       pixel.classList.toggle('pixel');
-      pixel.classList.toggle('noselect') // Prevents the 'ghosting' effect.
+      pixel.classList.toggle('noselect'); // Prevents the 'ghosting' effect.
     };
-    board.appendChild(row);
+    board.appendChild(column);
   };
 }
 
@@ -31,3 +30,6 @@ function fillPixel(elem) {
 }
 
 buildBoard();
+
+//********************* 
+// Todo: select color to paint with.
