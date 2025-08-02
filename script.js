e17d5700294aa3bce823ebba38f64a45ef69ae35
HuntOreo <hloreair@outlook.com>
2025-08-02 01:10:31 -0400

    Paint on canvas

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ const board = document.querySelector('.board');
 function buildBoard() {
   const boardSize = 50;
 
+  // Build board, one row at a time.
   for (let i = 0; i < boardSize; i++) {
     const row = document.createElement('div');
     row.classList.toggle('row');
     for (let j = 0; j < boardSize; j++) {
       const pixel = document.createElement('div');
+
+      // Listen for when a pixel is painted.
+      pixel.addEventListener('click', event => fillPixel(event.target))
+      pixel.addEventListener('mouseover', (event) => {
+        if (event.buttons == 1) {
+          fillPixel(event.target);
+        }
+      });
+
       row.appendChild(pixel);
       pixel.classList.toggle('pixel');
+      pixel.classList.toggle('noselect') // Prevents the 'ghosting' effect.
     };
     board.appendChild(row);
   };
+}
 
+function fillPixel(elem) {
+  elem.style.backgroundColor = 'black';
 }
 
 buildBoard();
