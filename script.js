f7c3addeaf4c46b1a7ff465d75dd37e4ff54e120
HuntOreo <hloreair@outlook.com>
2025-08-02 23:28:00 -0400

    Erase paint

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const board = document.querySelector('.board');
+const eraser = document.querySelector('.eraser');
+// Colors //
+const _ERASER = '#2f4f4f';
+const _RED = '#f00';
+const _GREEN = '#0f0';
+const _BLUE = '#00f';
+const _YELLOW = '#ff0';
+const _BLACK = '#000';
+const _WHITE = '#fff';
+const _CUSTOM_COLOR = '#000';
+let _CURRENT_COLOR = _BLACK;
 
 function buildBoard() {
   const boardSize = 50;
@@ -25,11 +36,18 @@ function buildBoard() {
   };
 }
 
+eraser.addEventListener('click', (event) => erasePixel(event.target))
+
 function fillPixel(elem) {
-  elem.style.backgroundColor = 'black';
+  elem.style.backgroundColor = _CURRENT_COLOR;
+}
+
+function erasePixel(elem) {
+  _CURRENT_COLOR = _ERASER;
 }
 
 buildBoard();
 
 //********************* 
+// Todo: Eraser functionality 
 // Todo: select color to paint with.
